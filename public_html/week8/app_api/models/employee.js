7a97baddcbd243cf033b99787607511d0906b78e
angelf331 <angelf331@gmail.com>
2016-12-11 15:07:23 -0500

    Add files via upload

---
+++ public_html/week8/app_api/models/employee.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+
+var employeeSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {
+        type: String,
+        required: true
+    },
+    department: {type: String, required: true},
+    startDate: {
+        type: String,
+        "default": Date.now
+    },
+    jobTitle: {type: String, required: true},
+    salary: {type: String, required: true}
+});
+
+/* This model will also create the collection in the Loc8r database when used */
+var Employee = mongoose.model('Employee', employeeSchema);
